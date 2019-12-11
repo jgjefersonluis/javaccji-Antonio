bc5a4daa04cad67ca8091dc6af95256c96ee374d
Jeferson Luis <jgjefersonluis@gmail.com>
2019-12-11 00:58:15 -0300

    inicio ccji-Antonio dois programas.

---
+++ 1Disciplina/src/unidade1/HelloWorldApp.java
@@ -0,0 +1,13 @@
+package unidade1;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		//System.out.println ("Hello, world!");
+		JOptionPane.showMessageDialog(null, "Hello, world!");
+
+	}
+
+}
---
+++ 1Disciplina/src/unidade2/ExemploApp.java
@@ -0,0 +1,25 @@
+package unidade2;
+
+import javax.swing.JOptionPane;
+
+public class ExemploApp {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * System.out.println("mensagem 1");
+		 * System.out.println("mensagem 2");
+		 * System.out.println("mensagem 3");
+		 * System.out.println("mensagem 4");
+		 * 
+		 */
+		JOptionPane.showMessageDialog(null,"mensagem 1");
+		JOptionPane.showMessageDialog(null,"mensagem 2");
+		JOptionPane.showMessageDialog(null,"mensagem 3");
+		JOptionPane.showMessageDialog(null,"mensagem 4");
+	
+		
+
+	}
+
+}
